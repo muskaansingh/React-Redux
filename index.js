e57e7f909e27df8266dab0d49c01d92c043a0114
Muskaan <muskaansingh235@gmail.com>
2021-03-21 11:55:46 +0530

    Added Store and action method

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //console.log('From index.js');
 
+const redux = require('redux');
+const createStore = redux.createStore;
+
+
 
 // Action
 
 const BUY_CAKE = 'BUY_CAKE'
-{
-    type: BUY_CAKE
-    info: 'First Redux Action'
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First Redux Action'
+    }
 }
+
 
 // State
 
@@ -26,3 +34,19 @@ const reducer = (state = initialState, action) => {
         default: return state
     }
 }
+
+const store = createStore (reducer);
+
+console.log('Initial State', store.getState());
+
+store.unsubscribe = store.unsubscribe(() => {
+    console.log('Updated State', store.getState())
+});
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
