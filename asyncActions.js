4e8da0c0a8d753fd674f939edb62c3437d871c00
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-05 11:41:25 +0530

    completed fetch a list of users from an API

--- asyncActions.js
+++ asyncActions.js
@@ -2,8 +2,13 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
+// 6.1 Apply middleware
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require('redux-thunk').default;  // passing as a argument of applyMiddleware to redux-store
 
 
+// 7. Import axios for getting data from the api
+const axios = require('axios');
 
 // 1. initialize the state
 const initialState = {
@@ -61,6 +66,35 @@ const reducer = (state=initialState, action) => {
     }
 }
 
-// 5.2 Create Redux Store
+// 8 Final step: is to create "async action creator" (action creator returms an function instead of action object)
 
-const store = createStore(store);
+// special about this function is that it doesn't have to be pure, so it allows to have side effects such as async API calls. 
+// This func also dispatches an actions, this is possible only bcz it receives dispatch method as argument
+const fetchUsers = () => {
+    return function(dispatch) {
+        // before fire of API call, first dispatch fetchUsersRequest which sets the loading true!
+        dispatch(fetchUsersRequest());
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                //response.data is the array of users
+                const users = response.data.map(user => user.id)
+                dispatch(fetchUsersSuccess(users));
+            })
+            .catch(error => {
+                //error.message
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+
+
+
+// 5.2 Create Redux Store  6.2 passing as a second parameter of redux-store
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+// 9. Finally subscribe to the store
+store.subscribe(() => {console.log(store.getState())})
+
+//10. dispatch async action creator
+store.dispatch(fetchUsers());
